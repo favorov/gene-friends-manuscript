523b576d41051621c6afb2393872d65ded33a445
Sasha Favorov <favorov@sensi.org>
2023-03-28 12:05:33 -0400

    plot in 9 panes, strangee

--- supplement/randomdiagmatrix.r
+++ supplement/randomdiagmatrix.r
@@ -1,5 +1,6 @@
 library(data.table)
 library(dplyr)
+library(ggplot2)
 
 msize<-100
 n<-1000
@@ -15,7 +16,9 @@ for (i in 1:n) {
   two_columns[i,1]<-msize-rank(matr[,1])[3]
   two_columns[i,2]<-msize-rank(matr[,2])[3]
 }
-toplot<-two_columns[, .(count=.N), by=.(u31, u32)]
+
+#toplot<-two_columns[, .(count=.N), by=.(u31, u32)]
+two_columns<-two_columns %>% count (u31,u32)
 plot(two_columns,cex=.5)
 tst<-cor.test(two_columns[[1]],two_columns[[2]],method="kendall")
 print(tst)
