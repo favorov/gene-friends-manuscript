e49fd8738c633ff7c77602455b89ebdef05da507
Alexander Favorov <favorov@sensi.org>
2023-03-28 01:06:26 -0400

    calculate points by .count

--- supplement/randomdiagmatrix.r
+++ supplement/randomdiagmatrix.r
@@ -15,6 +15,7 @@ for (i in 1:n) {
   two_columns[i,1]<-msize-rank(matr[,1])[3]
   two_columns[i,2]<-msize-rank(matr[,2])[3]
 }
+toplot<-two_columns[, .(count=.N), by=.(u31, u32)]
 plot(two_columns,cex=.5)
 tst<-cor.test(two_columns[[1]],two_columns[[2]],method="kendall")
 print(tst)
